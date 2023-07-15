ae4652674c8dda44a4e57184612171c869a7ad9a
ssh5340 <113916315+ssh5340@users.noreply.github.com>
2023-07-15 17:07:18 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1024 ms, Memory: 289604 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException, NumberFormatException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(bf.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String q = bf.readLine();
+
+            String[] qL = q.split(" ");
+            int a = Integer.parseInt(qL[0]);
+            int b = Integer.parseInt(qL[1]);
+
+            int result = a + b;
+
+            bw.write(result + "\n");
+        }
+        bw.flush();
+        bf.close();
+        bw.close();
+    }
+}
